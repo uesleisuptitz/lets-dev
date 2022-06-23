32191f7203e8ac3c216be7393dfff5a6a91bf85b
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2022-06-23 20:32:10 -0300

    roteamento + contexto + localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import {
-  FormBasico,
-  FormCompleto,
-  Callback,
-  Promise,
-  AsyncAwait,
-  Home,
-  ConsumindoApis,
-} from "./telas";
+import { FormBasico, FormCompleto, Home, ConsumindoApis } from "./telas";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MyContext } from "./contexto";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <Home />;
+  const [nomeUsuario, setNomeUsuario] = useState<string>();
+
+  useEffect(() => {
+    if (!nomeUsuario) {
+      let nomeDoLocalStorage = localStorage.getItem("nomeUsuario");
+      if (nomeDoLocalStorage) {
+        setNomeUsuario(nomeDoLocalStorage);
+      } else {
+        let nomeInformado = prompt("Qual o seu nome?");
+        if (nomeInformado) setNomeUsuario(nomeInformado);
+      }
+    } else {
+      localStorage.setItem("nomeUsuario", nomeUsuario);
+    }
+  }, [nomeUsuario]);
+
+  return (
+    <MyContext.Provider value={{ nomeUsuario, setNomeUsuario }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form-basico" element={<FormBasico />} />
+          <Route path="/form-completo" element={<FormCompleto />} />
+          <Route path="/consumindo-apis" element={<ConsumindoApis />} />
+        </Routes>
+      </BrowserRouter>
+    </MyContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/contexto/index.ts
@@ -0,0 +1,3 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+export const MyContext = React.createContext<any>({});
