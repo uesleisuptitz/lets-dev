a872b0faca6a35c1e55094c20f0fb383e2d4323b
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2022-06-30 14:58:53 -0300

    feat: use ref example

--- src/telas/home/home.tsx
+++ src/telas/home/home.tsx
@@ -1,124 +1,48 @@
-import * as s from "./styled-home";
-import {
-  Footer,
-  Header,
-  TituloSecao,
-  Card,
-  InputButton,
-} from "../../componentes";
-import { Logo } from "../../imagens";
-import { useContext } from "react";
-import { MyContext } from "../../contexto";
+import React, { useRef, FormEvent } from "react";
 
-const Home = () => {
-  const { setNomeUsuario, nomeUsuario } = useContext(MyContext);
+const Home: React.FC = () => {
+  const formRef = useRef({
+    nome: "",
+    sobrenome: "",
+    idade: "",
+    count: 0,
+  });
 
-  const telas = [
-    {
-      titulo: "Formulário em HTML e CSS",
-      link: "/form-basico",
-      descricao: (
-        <p>
-          O primeiro <i>“Hello World”</i> a gente nunca esquece. O primeiro
-          formulário feito na aula de Introdução ao HTML e CSS.
-        </p>
-      ),
-    },
-    {
-      titulo: "Formulário 2.0",
-      link: "/form-completo",
-      descricao: (
-        <p>
-          O primeiro <i>“console.log”</i> a gente também não esquece. O
-          formulário funcional e agora com React JS + HTML + CSS.
-        </p>
-      ),
-    },
-    {
-      titulo: "Consumindo API's",
-      link: "/consumindo-apis",
-      descricao:
-        "Brincando com a API viaCEP, informando um CEP, salvando os valores digitados no estado e mostrando os dados na tela.",
-    },
-    {
-      titulo: "A PAIPE e os Paipers",
-      link: "/sobre-paipers",
-      descricao:
-        "Uma página bônus de presente para deixar o seu projeto ainda mais completo. E de quebra ficar por dentro do que acontece na Paipe.",
-    },
-  ];
+  const mostrarValores = (event: FormEvent) => {
+    event.preventDefault();
+    if (formRef.current) {
+      formRef.current.count += 1;
+      console.log("formRef", formRef.current);
+    }
+  };
 
   return (
-    <s.Container>
-      <Header />
-      <s.Banner>
-        <h1>Que prazer te ver aqui, {nomeUsuario}!</h1>
-        <img src={Logo} />
-        <h2>
-          Seja bem-vindo(a) ao Let’s Dev! O evento que vai transformar sua
-          carreira.
-        </h2>
-        <p>
-          Você está no módulo Front-end e vai aprender a criar um site básico do
-          zero.
-        </p>
-      </s.Banner>
-      <s.Content>
-        <TituloSecao titulo="O que fizemos até agora..." />
-
-        <s.GroupCards>
-          {telas.map((card) => (
-            <Card
-              titulo={card.titulo}
-              link={card.link}
-              descricao={card.descricao}
-            />
-          ))}
-        </s.GroupCards>
-
-        <TituloSecao titulo="Um pouco sobre mim..." />
-
-        <s.Row>
-          <img src={Logo} />
-          <s.Column>
-            <h3>Aqui vai o nome do(a) Dev</h3>
-            <p>
-              Este espaço é destinado à sua descrição de perfil. Conta aqui quem
-              você é, gostos, hobbies e o que achar interessante. Descreve
-              também as características profissionais e o que te motivou a
-              ingressar na área do desenvolviemento de software.
-            </p>
-            <p>
-              Você pode substituir a foto a lado pela sua melhor foto e colocar
-              suas redes nos botões abaixo!
-            </p>
-            <s.ButtonGroup>
-              <InputButton
-                outlined
-                type="button"
-                value="LinkedIn"
-                onClick={() => {
-                  //Aqui vai o seu perfil do linkedIn
-                  window.open("https://www.linkedin.com", "_blank");
-                }}
-                title="Vai para o perfil no LinkedIn"
-              />
-              <InputButton
-                type="submit"
-                value="Ver perfil"
-                onClick={() => {
-                  //Aqui vai o seu perfil do GitHub
-                  window.open("https://github.com", "_blank");
-                }}
-                title="Vai para o perfil no GitHub"
-              />
-            </s.ButtonGroup>
-          </s.Column>
-        </s.Row>
-      </s.Content>
-      <Footer />
-    </s.Container>
+    <div>
+      <form onSubmit={mostrarValores}>
+        <input
+          type="text"
+          name="nome"
+          onChange={(event) => (formRef.current.nome = event.target.value)}
+        />
+        <input
+          type="text"
+          name="sobrenome"
+          onChange={(event) => (formRef.current.sobrenome = event.target.value)}
+        />
+        <input
+          type="number"
+          name="idade"
+          onChange={(event) => (formRef.current.idade = event.target.value)}
+        />
+        <button type="submit">Mostrar valores</button>
+      </form>
+      <Logger />
+    </div>
   );
+};
+
+const Logger: React.FC = () => {
+  return <div></div>;
 };
 
 export default Home;
